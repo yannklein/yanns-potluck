9878000b4927e9c4bcc1235c48371208314a43d7
Yann Klein <yann.klein@me.com>
2022-11-27 16:50:12 +0900

    created potluck controller and routes

---
+++ app/controllers/potluck_controller.rb
@@ -0,0 +1,20 @@
+class PotluckController < ApplicationController
+  before_action :set_potluck, only: [:show]
+
+  def index
+    @potlucks = Potluck.all
+  end
+
+  def show
+
+  end
+
+  def create!
+  end
+
+  private
+
+  def set_potluck
+    @potluck = Potluck.find(params[:id])
+  end
+end
---
+++ test/controllers/potluck_controller_test.rb
@@ -0,0 +1,7 @@
+require "test_helper"
+
+class PotluckControllerTest < ActionDispatch::IntegrationTest
+  # test "the truth" do
+  #   assert true
+  # end
+end
