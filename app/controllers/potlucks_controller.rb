9c6b072c9e859dcf4dbd80b73cfd36fdc41d79fe
Yann Klein <yann.klein@me.com>
2022-11-28 16:41:14 +0900

    potluck creation and destruction logic

--- app/controllers/potlucks_controller.rb
+++ app/controllers/potlucks_controller.rb
@@ -1,10 +1,10 @@
 class PotlucksController < ApplicationController
-  before_action :set_potluck, only: [:show]
+  before_action :set_potluck, only: [:show, :destroy]
 
   def index
     @potlucks = 
-      Potluck.joins(:stuffs).where(user: current_user)
-      .or(Potluck.joins(:stuffs).where(stuffs: {user: current_user}))
+      Potluck.left_joins(:stuffs).where(user: current_user)
+      .or(Potluck.left_joins(:stuffs).where(stuffs: {user: current_user}))
       .distinct
   end
 
@@ -12,12 +12,28 @@ class PotlucksController < ApplicationController
 
   end
 
-  def create!
+  def create
+    @potluck = Potluck.new(potluck_params)
+    @potluck.user = current_user
+    if @potluck.save
+      redirect_to potluck_path(@potluck)
+    else
+      render 'index'
+    end
+  end
+
+  def destroy
+    @potluck.destroy
+    redirect_to potlucks_path
   end
 
   private
 
   def set_potluck
     @potluck = Potluck.find(params[:id])
+  end
+
+  def potluck_params
+    params.require(:potluck).permit(:name, :date, :location, :apt_number )
   end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,7 @@
 Rails.application.routes.draw do
   devise_for :users
   root to: "pages#home"
-  resources :potlucks, only: [:index, :show] do
+  resources :potlucks, only: [:index, :show, :create, :destroy] do
     resources :stuffs, only: [:create]
   end
   resources :stuffs, only: [:update, :destroy]
